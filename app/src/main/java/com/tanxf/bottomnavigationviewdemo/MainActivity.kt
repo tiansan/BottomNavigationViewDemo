8c3f4f76783c3f8ebb2f9247a02aca7137316ac6
tanxf <tanxf@amtxts.com>
2018-12-18 10:39:21 +0800

    Initial commit

---
+++ app/src/main/java/com/tanxf/bottomnavigationviewdemo/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.tanxf.bottomnavigationviewdemo
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        navigation.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when (p0.itemId) {
+            R.id.action_search -> return true
+            R.id.action_settings -> return true
+            R.id.action_navigation -> return true
+            R.id.action_me -> return true
+        }
+        return false
+    }
+
+
+}
